d10a84d05e328b7f216f93edd3a3209189299130
JoachimDittman <JoachimDittman@10.50.130.155>
2015-03-10 11:52:10 +0100

    Zipcodes SQL addet

--- ex4/src/java/Entity/CityInfo.java
+++ ex4/src/java/Entity/CityInfo.java
@@ -17,14 +17,14 @@ import javax.persistence.Id;
  */
 @Entity
 public class CityInfo implements Serializable {
-    private static final long serialVersionUID = 1L;
+  
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private Long zipcode;
+    
+  
+    private String zipcode;
     private String city; 
 
-    public CityInfo(Long zipcode, String city) {
+    public CityInfo(String zipcode, String city) {
         this.zipcode = zipcode;
         this.city = city;
     }
@@ -32,11 +32,11 @@ public class CityInfo implements Serializable {
     public CityInfo() {
     }
 
-    public Long getZipcode() {
+    public String getZipcode() {
         return zipcode;
     }
 
-    public void setZipcode(Long zipcode) {
+    public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
 
@@ -47,39 +47,5 @@ public class CityInfo implements Serializable {
     public void setCity(String city) {
         this.city = city;
     }
-    
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof CityInfo)) {
-            return false;
-        }
-        CityInfo other = (CityInfo) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Entity.CityInfo[ id=" + id + " ]";
-    }
-    
+   
 }
